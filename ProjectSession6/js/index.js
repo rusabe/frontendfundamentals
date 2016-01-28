f7dcfb277e67ce3e5a3640250a69df41db8a479f
Rubén Sáez <ruben@MacBook-Pro-de-Ruben.local>
2016-01-28 01:03:53 +0100

    Finished Pomodoro clock Project Session 6

--- ProjectSession6/js/index.js
+++ ProjectSession6/js/index.js
@@ -20,47 +20,95 @@ function subtractSession(){
     sessionField.innerHTML = Number(sessionField.innerHTML)-1;
 }
 
-var second = document.getElementById("second").innerHTML;
+var second;
 var minute;
 var hundredsofsecond;
 var interval;
+var isSession = true;
+var totalSeconds;
 function start(){
     hundredsofsecond = 100;
     second = 60;
+    minute = Number(document.getElementById("sessionValue").innerHTML);
+    totalSeconds = Number(minute)*60;
 
     interval = setInterval(function(){
-        hundredsofsecond--;
-        if(hundredsofsecond<10)
-        {
-            hundredsofsecond = "0" + hundredsofsecond;
-        }
-
-        document.getElementById("hundredsofsecond").innerHTML = hundredsofsecond;
-        if(hundredsofsecond == 99)
-        {
-            if(second>0)
+        if(Number(hundredsofsecond) == 0 && Number(second) == 0 && minute == 0){
+            isSession = !isSession;
+            if(isSession)
             {
-                second--;
-                if(second<10)
-                {
-                    second = "0" + second;
-                }
-                document.getElementById("second").innerHTML = second;
+                minute = Number(document.getElementById("sessionValue").innerHTML);
+                document.getElementById("isBreak").innerHTML = 'SESSION';
             }
-        }
-        
+            else
+            {
+                minute = Number(document.getElementById("breakValue").innerHTML);
+                document.getElementById("isBreak").innerHTML = 'BREAK';
+            }
+            totalSeconds = Number(minute)*60;
+            second = 60;
+            hundredsofsecond = 100;
+        } 
+        else
+        {
+            if(hundredsofsecond == 0)
+            {
+                hundredsofsecond = 100;
+            }
+            hundredsofsecond--;
+            if(Number(hundredsofsecond)<10)
+            {
+                document.getElementById("hundredsofsecond").innerHTML = "0" + hundredsofsecond;
+            }
+            else
+            {
+                document.getElementById("hundredsofsecond").innerHTML = hundredsofsecond;
+            }
 
+            if(Number(hundredsofsecond) == 99)
+            {
+                if(second==0)
+                {
+                    second = 60;
+                }
+                second--;
+                if(Number(second)<10)
+                {
+                    document.getElementById("second").innerHTML = "0" + second;
+                }
+                else
+                {
+                    document.getElementById("second").innerHTML = second;
+                }
+                
+            }
+            console.log(Number(second) + ':' + Number(hundredsofsecond));
+            if(Number(second) == 59 && Number(hundredsofsecond) == 99)
+            {
+                minute--;
+                if(Number(minute)<10)
+                {
+                    document.getElementById("minute").innerHTML = "0" + minute;
+                }
+                else
+                {
+                    document.getElementById("minute").innerHTML = minute;
+                }
+            }
+        }     
 
-        if(hundredsofsecond == 0 && second == 0){
-            clearInterval(interval);
-        }        
+        var actualSeconds = minute*60 + second;
+        var percentage_current = actualSeconds / totalSeconds * 100;
+        document.getElementById("progressBar").style.background = 'linear-gradient(yellow '+percentage_current+'%,#a0c884 '+percentage_current+'%)';
     }, 10);
 }
 
 function stop(){
     clearInterval(interval);
-    document.getElementById("second").innerHTML = "00";
     document.getElementById("minute").innerHTML = "00";
+    document.getElementById("second").innerHTML = "00";
     document.getElementById("hundredsofsecond").innerHTML = "00";
+    document.getElementById("isBreak").innerHTML = 'SESSION';
+    document.getElementById("progressBar").style.background = 'yellow';
 
 }
